672b4c96d29c5a985947f54933756b7a3c985f48
August Dahlkvist <AugustDahlkvist@gmail.com>
2024-11-23 23:53:02 +0100

    Init commit

---
+++ Game1.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+
+namespace SongProblem
+{
+	public class Game1 : Game
+	{
+		private GraphicsDeviceManager _graphics;
+
+		private Song mSong;
+		private SoundEffect mSongAsSoundEffect;
+		private SoundEffectInstance mSFXInstance;
+
+		public Game1()
+		{
+			_graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+			IsMouseVisible = true;
+		}
+
+		protected override void Initialize()
+		{
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+			mSong = Content.Load<Song>("Song");
+			mSongAsSoundEffect = Content.Load<SoundEffect>("SongAsSFX");
+
+			// Change me to see how MediaPlayer and SoundEffect differ.
+			const bool playSongAsSFX = true;
+
+			if(playSongAsSFX)
+			{
+				PlaySongAsSFX();
+			}
+			else
+			{
+				PlaySong();
+			}
+		}
+
+		private void PlaySong()
+		{
+			MediaPlayer.Volume = 0.3f;
+			MediaPlayer.IsRepeating = true;
+
+			MediaPlayer.Play(mSong);
+		}
+
+		private void PlaySongAsSFX()
+		{
+			mSFXInstance = mSongAsSoundEffect.CreateInstance();
+			mSFXInstance.IsLooped = true;
+			mSFXInstance.Volume = 0.3f;
+
+			mSFXInstance.Play();
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.Black);
+
+			base.Draw(gameTime);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new SongProblem.Game1();
+game.Run();
